6a48c54aa572b4621e1a85786d05c2d66f933695
sumanyaselvi-r <sumanyaselvi@gmail.com>
2023-12-24 08:43:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,86 @@
+
+
+require('dotenv').config();
+const cors = require('cors');
+const nodemailer=require('nodemailer')
+
+const express =require('express')
+const bodyParser=require('body-parser')
+
+const app=express();
+app.use(cors({origin:`${process.env.CLIENT_URL}`}));
+
+const PORT =process.env.PORT ||3001
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.post('/send-email',(req,res)=>{
+   
+    
+    
+    try{
+        const data= req.body;
+        console.log(data)
+        const transporter=nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            }
+        })
+        const options={
+            from: 'Company',
+            to: process.env.SEND_TO,
+            subject:'message from client',
+            html:`<html>
+            <head>
+              <style>
+                body {
+                  font-family: PT serif;
+                  background-color: #f4f4f4;
+                  color: #333;
+                }
+                h1 {
+                  color: #007bff;
+                }
+                p {
+                  margin-bottom: 10px;
+                }
+              </style>
+            </head>
+            <body>
+              <h1>Contact Form Submission</h1>
+              <p><strong>Name:</strong> ${data.name}</p>
+              <p><strong>Email:</strong> ${data.email}</p>
+              <p><strong>Phoneno:</strong> ${data.phone}</p>
+              <p><strong>Message:</strong> ${data.message}</p>
+            </body>
+          </html>`
+        }
+        transporter.sendMail(options,(err,info)=>{
+            if(err)
+            {
+                console.log(err)
+                return
+            }
+            else{
+                console.log(`Email sent:${info.res}`)
+            }
+        
+         })
+
+        res.json({msg:'your message sent successfully'})
+
+    }catch(error){
+        res.status(404).json({msg:"error"})
+
+    }
+ 
+
+  
+    });
+    
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+})
